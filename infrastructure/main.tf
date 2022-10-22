de41bab29fa1f599357ad0a56e94385d3de24885
JC <57596836+jc-tapia@users.noreply.github.com>
2022-10-21 21:45:59 -0300

    add terraform infra

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,31 +1,44 @@
-resource "azurerm_resource_group" "rg" {
-  name     = var.resource_group_name
-  location = var.location
-}
-
-resource "azurerm_kubernetes_cluster" "aks" {
-  name                = var.cluster_name
-  kubernetes_version  = var.kubernetes_version
-  location            = var.location
-  resource_group_name = azurerm_resource_group.rg.name
-  dns_prefix          = var.cluster_name
-  node_resource_group = var.node_resource_group
-
-  default_node_pool {
-    name                = "system"
-    node_count          = var.system_node_count
-    vm_size             = "Standard_DS2_v2"
-    type                = "VirtualMachineScaleSets"
-    availability_zones  = [1, 2, 3]
-    enable_auto_scaling = false
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=2.46.0"
+    }
   }
-
+    backend "azurerm" {
+        resource_group_name  = "tfresourcegroup"
+        storage_account_name = "tfstorageaccount"
+        container_name       = "tfcontainer"
+        key                  = "terraform.tfstate"
+    }
+}
+resource "azurerm_resource_group" "rg" {
+  name     = "resourceGroupAks"
+  location = "West Europe"
+}
+resource "azurerm_container_registry" "acr" {
+  name                = "containerRegistry"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  sku                 = "Premium"
+}
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = "kubernetesCluster"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  dns_prefix          = "dnsaks"
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_D2_v2"
+  }
   identity {
     type = "SystemAssigned"
   }
-
-  network_profile {
-    load_balancer_sku = "Standard"
-    network_plugin    = "kubenet" # azure (CNI)
-  }
+}
+resource "azurerm_role_assignment" "example" {
+  principal_id                     = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
+  role_definition_name             = "AcrPull"
+  scope                            = azurerm_container_registry.acr.id
+  skip_service_principal_aad_check = true
 }
