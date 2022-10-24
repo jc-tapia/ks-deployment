e9e71ba9d68d54f8f4feb53ad82638940db4a894
JC <57596836+jc-tapia@users.noreply.github.com>
2022-10-23 22:33:16 -0300

    rename resources

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -12,17 +12,17 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "rg" {
-  name     = "resourceGroupAks"
+  name     = "myJcResourceGroupAks"
   location = "East US 2"
 }
 resource "azurerm_container_registry" "acr" {
-  name                = "myContainerRegistry"
+  name                = "myJcContainerRegistry"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
   sku                 = "Premium"
 }
 resource "azurerm_kubernetes_cluster" "aks" {
-  name                = "kubernetesCluster"
+  name                = "MyJckubernetesCluster"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   dns_prefix          = "dnsaks"
