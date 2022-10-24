c928a3d6cb92bda7e88a6e821bd503e76bd54470
JC <57596836+jc-tapia@users.noreply.github.com>
2022-10-24 10:56:05 -0300

    update infra

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -19,8 +19,7 @@ resource "azurerm_container_registry" "acr" {
   name                = "myJcContainerRegistry"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
-  sku                 = "Basic"
-  network_rule_set    = []
+  sku                 = "Standard"
 }
 resource "azurerm_kubernetes_cluster" "aks" {
   name                = "MyJckubernetesCluster"
@@ -38,7 +37,7 @@ resource "azurerm_kubernetes_cluster" "aks" {
 }
 resource "azurerm_role_assignment" "role" {
   principal_id                     = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
-  role_definition_name             = "Contributor"
+  role_definition_name             = "AcrPull"
   scope                            = azurerm_container_registry.acr.id
   skip_service_principal_aad_check = true
 }
