c0867d058740d87712528e47b8c0c9485d854792
JC <57596836+jc-tapia@users.noreply.github.com>
2022-10-25 22:27:30 -0300

    remove infra

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -11,33 +11,33 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "rg" {
-  name     = "myJcResourceGroupAks"
-  location = "East US 2"
-}
-resource "azurerm_container_registry" "acr" {
-  name                = "myJcContainerRegistry"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
-  sku                 = "Standard"
-}
-resource "azurerm_kubernetes_cluster" "aks" {
-  name                = "MyJckubernetesCluster"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  dns_prefix          = "dnsjc"
-  default_node_pool {
-    name       = "default"
-    node_count = 1
-    vm_size    = "Standard_D11"
-  }
-  identity {
-    type = "SystemAssigned"
-  }
-}
-resource "azurerm_role_assignment" "role" {
-  principal_id                     = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
-  role_definition_name             = "AcrPull"
-  scope                            = azurerm_container_registry.acr.id
-  skip_service_principal_aad_check = true
-}
+# resource "azurerm_resource_group" "rg" {
+#   name     = "myJcResourceGroupAks"
+#   location = "East US 2"
+# }
+# resource "azurerm_container_registry" "acr" {
+#   name                = "myJcContainerRegistry"
+#   resource_group_name = azurerm_resource_group.rg.name
+#   location            = azurerm_resource_group.rg.location
+#   sku                 = "Standard"
+# }
+# resource "azurerm_kubernetes_cluster" "aks" {
+#   name                = "MyJckubernetesCluster"
+#   location            = azurerm_resource_group.rg.location
+#   resource_group_name = azurerm_resource_group.rg.name
+#   dns_prefix          = "dnsjc"
+#   default_node_pool {
+#     name       = "default"
+#     node_count = 1
+#     vm_size    = "Standard_D11"
+#   }
+#   identity {
+#     type = "SystemAssigned"
+#   }
+# }
+# resource "azurerm_role_assignment" "role" {
+#   principal_id                     = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
+#   role_definition_name             = "AcrPull"
+#   scope                            = azurerm_container_registry.acr.id
+#   skip_service_principal_aad_check = true
+# }
